cc2bcab9b45902ea2f6419955b2c63767c2666bb
PrateekMahawar <Prateek.mahawar@cliffex.com>
2018-09-18 09:27:45 +0530

    List

---
+++ AccessControl.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+/*
+ 1. private
+ 2. fileprivate - Avaible in that file only
+ 3. internal - By default(Everything we dont see before init)(Availbe within that module)
+ 4. public - Cannot be overriden
+ 5. open - can be overriden
+ */
+
+//Ex 1) Game Application
+
+class Game {
+    private var number = 0
+    
+    var score: Int {
+        return number
+    }
+    
+    func increaseByOne() {
+        number += 1
+    }
+}
+// Inaccessible outside class
+//Game().number = 10
+
+//Ex 2) Singleton
+
+class AccountManager {
+    static let sharedInstance = AccountManager()
+    var userInfo = (ID: "prateekMahawar", Password: 22222222)
+//Init protecting the code
+    private init () {}
+}
+//Inaccessible
+//AccountManager()
+
+//Swift 4 Change
+
+class Status  {
+    private var isHappy = true
+}
+//Not available outside of class definition before swift 4
+extension Status {
+    func checkStatus() {
+        print(isHappy)
+    }
+}
